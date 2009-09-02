2efcf8c281f6779cbe99dee6a88a32d1a1236278
Kazuki UCHIDA <gioext@gmail.com>
2009-09-02 16:08:28 +0900

    fixed

--- resize.c
+++ resize.c
@@ -1,83 +1,11 @@
 #include "resize.h"
 
-static int
-gcd(int x, int y)
-{
-    int m, n, r;
-    m = x > y ? x : y;
-    n = x < y ? x : y;
+static int gcd(int x, int y);
+static int lcm(int x, int y);
+static double sinc(double x);
+static double lanczos(double x, int n);
+static int filter(double x, double total);
 
-    if (n == 0) {
-        return m;
-    }
-
-    while(1) {
-        r = m % n;
-        if (r == 0) {
-            return n;
-        }
-        m = n;
-        n = r;
-    }
-}
-
-static int
-lcm(int x, int y)
-{
-    return (x * y) / gcd(x, y);
-}
-
-/*
- * lanczos func
- * sinc(x) * sinc(x / n)
- *
- * sinc func
- * sin(PI * x) / (PI * x)
- *
- */
-static double
-sinc(double x)
-{
-    return sin(PI * x) / (PI * x);
-}
-
-static double
-lanczos(double x, int n)
-{
-    if (n < 2 || n > 3) {
-        fprintf(stderr, "Only suport lanczos-2 or lanczos-3\n");
-        exit(1);
-    }
-    static double lookup[DISCRETE_SIZE];
-    int i = (int)(x * 1000);
-
-    if (i <= 0) {
-        return 1.0;
-    } else if (i >= DISCRETE_SIZE) {
-        return 0.0;
-    }
-
-
-    double r = lookup[i];
-    if (r == 0.0) {
-        r = sinc(x) * sinc(x / n);
-        lookup[i] = r;
-    }
-    return r;
-}
-
-static int
-filter(double x, double total)
-{
-    int c = x / total;
-    if (c < 0) {
-        return 0;
-    } else if (c > 255) {
-        return 255;
-    } else {
-        return c;
-    }
-}
 
 GIC_IMAGE *
 gic_resize_nearest_neighbour(GIC_IMAGE *img, double scale)
@@ -112,13 +40,17 @@ GIC_IMAGE *
 gic_resize_area_average(GIC_IMAGE *img, double scale)
 {
     int to_width, to_height, from_width, from_height;
+    int lcmx, lcmy;
+    double sx, sy, dx, dy, dxy;
+    double *R, *G, *B;
+
+
     from_width = img->width;
     from_height = img->height;
     to_width = img->width * scale;
     to_height = img->height * scale;
     GIC_IMAGE *to_img = gic_create_image(to_width, to_height);
 
-    int lcmx, lcmy;
     lcmx = lcm(to_width, from_width);
     lcmy = lcm(to_height, from_height);
     if (lcmx > MAX_SPLIT_SIZE * to_width) {
@@ -127,30 +59,36 @@ gic_resize_area_average(GIC_IMAGE *img, double scale)
     if (lcmy > MAX_SPLIT_SIZE * to_height) {
         lcmy = MAX_SPLIT_SIZE * to_height;
     }
-    double sx = (double)from_width / lcmx;
-    double sy = (double)from_height / lcmy;
-    double dx = (double)to_width / lcmx;
-    double dy = (double)to_height / lcmy;
-    double dxy = dx * dy;
+    sx = (double)from_width / lcmx;
+    sy = (double)from_height / lcmy;
+    dx = (double)to_width / lcmx;
+    dy = (double)to_height / lcmy;
+    dxy = dx * dy;
 
-    double *R = (double *)calloc(sizeof(double), to_width * to_height);
-    double *G = (double *)calloc(sizeof(double), to_width * to_height);
-    double *B = (double *)calloc(sizeof(double), to_width * to_height);
+    R = (double *)calloc(sizeof(double), to_width * to_height);
+    G = (double *)calloc(sizeof(double), to_width * to_height);
+    B = (double *)calloc(sizeof(double), to_width * to_height);
 
-    int w, h;
+
+    int w, h, fy, ty, tyw, fx, tx;
     for (h = 0; h < lcmy; h++) {
-        int fy = (int)(h * sy);
-        int ty = (int)(h * dy);
-        int tyw = ty * to_width;
+        fy = (int)(h * sy);
+        ty = (int)(h * dy);
+        tyw = ty * to_width;
         for (w = 0; w < lcmx; w++) {
-            int fx = (int)(w * sx);
-            int tx = (int)(w * dx);
+            fx = (int)(w * sx);
+            tx = (int)(w * dx);
 
-            R[tyw + tx] += img->data[fy][fx * 3 + 0] * dxy;
-            G[tyw + tx] += img->data[fy][fx * 3 + 1] * dxy;
-            B[tyw + tx] += img->data[fy][fx * 3 + 2] * dxy;
+            int fx3, tywtx;
+            fx3 = fx * 3;
+            tywtx = tyw + tx;
+
+            R[tywtx] += img->data[fy][fx3 + 0] * dxy;
+            G[tywtx] += img->data[fy][fx3 + 1] * dxy;
+            B[tywtx] += img->data[fy][fx3 + 2] * dxy;
         }
     }
+
     for (h = 0; h < to_height; h++) {
         for (w = 0; w < to_width; w++) {
             to_img->data[h][w * 3 + 0] = R[h * to_width + w];
@@ -233,3 +171,81 @@ gic_resize_lanczos(GIC_IMAGE *img, double scale, int n)
     return to_img;
 }
 
+static int
+gcd(int x, int y)
+{
+    int m, n, r;
+    m = x > y ? x : y;
+    n = x < y ? x : y;
+
+    if (n == 0) {
+        return m;
+    }
+
+    while(1) {
+        r = m % n;
+        if (r == 0) {
+            return n;
+        }
+        m = n;
+        n = r;
+    }
+}
+
+static int
+lcm(int x, int y)
+{
+    return (x * y) / gcd(x, y);
+}
+
+/*
+ * lanczos func
+ * sinc(x) * sinc(x / n)
+ *
+ * sinc func
+ * sin(PI * x) / (PI * x)
+ *
+ */
+static double
+sinc(double x)
+{
+    return sin(PI * x) / (PI * x);
+}
+
+static double
+lanczos(double x, int n)
+{
+    if (n < 2 || n > 3) {
+        fprintf(stderr, "Only suport lanczos-2 or lanczos-3\n");
+        exit(1);
+    }
+    static double lookup[DISCRETE_SIZE];
+    int i = (int)(x * 1000);
+
+    if (i <= 0) {
+        return 1.0;
+    } else if (i >= DISCRETE_SIZE) {
+        return 0.0;
+    }
+
+
+    double r = lookup[i];
+    if (r == 0.0) {
+        r = sinc(x) * sinc(x / n);
+        lookup[i] = r;
+    }
+    return r;
+}
+
+static int
+filter(double x, double total)
+{
+    int c = x / total;
+    if (c < 0) {
+        return 0;
+    } else if (c > 255) {
+        return 255;
+    } else {
+        return c;
+    }
+}
